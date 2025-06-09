cb8d16eb3536f679b18a3a6a0b954824f4a01d09
William Cruz <cruzwilliam14@gmail.com>
2025-06-09 17:17:13 -0300

    Upload configs

--- condomineo/settings.py
+++ condomineo/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DB', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOST', default='127.0.0.1').split(',')
 
 # Application definition
 INSTALLED_APPS = [
@@ -76,7 +76,6 @@ UNFOLD = {
         'base': {
             50: '#f8fafc',
             100: '#f1f5f9',
-            # ... outros tons
         },
         'font': {
             'subtle-light': '#9ca3af',
