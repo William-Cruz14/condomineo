0ebb5c8b18a5ff0fe15c525e8cbd71ee6d4e87a4
William Cruz <cruzwilliam14@gmail.com>
2025-06-09 16:40:28 -0300

    Update signals

--- users/apps.py
+++ users/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+    def ready(self):
+        pass
+        # Importando os sinais para garantir que sejam registrados
+        import users.signals
